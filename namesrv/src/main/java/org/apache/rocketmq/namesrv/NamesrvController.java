b7e79f345a886fc28fd7f4f1abfc459544d24fad
Gertent <wyf6666wyf@126.com>
2017-12-15 10:16:21 +0800

    init.........

---
+++ namesrv/src/main/java/org/apache/rocketmq/namesrv/NamesrvController.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.namesrv;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import org.apache.rocketmq.common.Configuration;
+import org.apache.rocketmq.common.ThreadFactoryImpl;
+import org.apache.rocketmq.common.constant.LoggerName;
+import org.apache.rocketmq.common.namesrv.NamesrvConfig;
+import org.apache.rocketmq.namesrv.kvconfig.KVConfigManager;
+import org.apache.rocketmq.namesrv.processor.ClusterTestRequestProcessor;
+import org.apache.rocketmq.namesrv.processor.DefaultRequestProcessor;
+import org.apache.rocketmq.namesrv.routeinfo.BrokerHousekeepingService;
+import org.apache.rocketmq.namesrv.routeinfo.RouteInfoManager;
+import org.apache.rocketmq.remoting.RemotingServer;
+import org.apache.rocketmq.remoting.netty.NettyRemotingServer;
+import org.apache.rocketmq.remoting.netty.NettyServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NamesrvController {
+    private static final Logger log = LoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
+
+    private final NamesrvConfig namesrvConfig;
+
+    private final NettyServerConfig nettyServerConfig;
+
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(new ThreadFactoryImpl(
+        "NSScheduledThread"));
+    private final KVConfigManager kvConfigManager;
+    private final RouteInfoManager routeInfoManager;
+
+    private RemotingServer remotingServer;
+
+    private BrokerHousekeepingService brokerHousekeepingService;
+
+    private ExecutorService remotingExecutor;
+
+    private Configuration configuration;
+
+    public NamesrvController(NamesrvConfig namesrvConfig, NettyServerConfig nettyServerConfig) {
+        this.namesrvConfig = namesrvConfig;
+        this.nettyServerConfig = nettyServerConfig;
+        this.kvConfigManager = new KVConfigManager(this);
+        this.routeInfoManager = new RouteInfoManager();
+        this.brokerHousekeepingService = new BrokerHousekeepingService(this);
+        this.configuration = new Configuration(
+            log,
+            this.namesrvConfig, this.nettyServerConfig
+        );
+        this.configuration.setStorePathFromConfig(this.namesrvConfig, "configStorePath");
+    }
+
+    public boolean initialize() {
+        //kvConfig.json的加载（只在启动的时候加载这个，没有就不加载了。路径在宿主目录下namesrv文件夹下）
+        this.kvConfigManager.load();
+        //NettyRemotingServer实例化（这里会作下当前系统的判断，是否为Linux，至于这里判断是否为linux或windows的操作也很简单，就是System.getProperty("os.name")，检查是否包含"linux"或"windows"）。作这些处理也不是为什么特别的操作，就是线程名字的相关处理，因为这里初始化了不少操作，也初始化相关的一些线程池）
+        this.remotingServer = new NettyRemotingServer(this.nettyServerConfig, this.brokerHousekeepingService);
+
+        this.remotingExecutor =
+            Executors.newFixedThreadPool(nettyServerConfig.getServerWorkerThreads(), new ThreadFactoryImpl("RemotingExecutorThread_"));
+
+        this.registerProcessor();
+
+        this.scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+
+            @Override
+            public void run() {
+                NamesrvController.this.routeInfoManager.scanNotActiveBroker();
+            }
+        }, 5, 10, TimeUnit.SECONDS);
+
+        this.scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+
+            @Override
+            public void run() {
+                NamesrvController.this.kvConfigManager.printAllPeriodically();
+            }
+        }, 1, 10, TimeUnit.MINUTES);
+
+        return true;
+    }
+
+    private void registerProcessor() {
+        if (namesrvConfig.isClusterTest()) {
+
+            this.remotingServer.registerDefaultProcessor(new ClusterTestRequestProcessor(this, namesrvConfig.getProductEnvName()),
+                this.remotingExecutor);
+        } else {
+
+            this.remotingServer.registerDefaultProcessor(new DefaultRequestProcessor(this), this.remotingExecutor);
+        }
+    }
+
+    public void start() throws Exception {
+        this.remotingServer.start();
+    }
+
+    public void shutdown() {
+        this.remotingServer.shutdown();
+        this.remotingExecutor.shutdown();
+        this.scheduledExecutorService.shutdown();
+    }
+
+    public NamesrvConfig getNamesrvConfig() {
+        return namesrvConfig;
+    }
+
+    public NettyServerConfig getNettyServerConfig() {
+        return nettyServerConfig;
+    }
+
+    public KVConfigManager getKvConfigManager() {
+        return kvConfigManager;
+    }
+
+    public RouteInfoManager getRouteInfoManager() {
+        return routeInfoManager;
+    }
+
+    public RemotingServer getRemotingServer() {
+        return remotingServer;
+    }
+
+    public void setRemotingServer(RemotingServer remotingServer) {
+        this.remotingServer = remotingServer;
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+}
